19dba64b7aaa02ee843cb67d09ad72a0d75f91bc
Alimacamila <contatocamilafrancatti@gmail.com>
2024-07-10 21:04:51 -0300

    Aula 2

--- README.md
+++ README.md
@@ -4,4 +4,10 @@ Este é um repositório dedicado ao evento NLW Journey da Rocketseat. Decidi seg
 ![wallpaper](/assets/wallpaper.png)
 
 ## 🛠️ Ferramentas e Objetivos
-Durante o evento, iremos utilizar conceitos e ferramentas de IA Generativa, consulta na Internet, LLM (Large Language Models), Python e suas Libs. Com o **objetivo** de formar um agente planejador de viagens, o qual consulta dados e responde requests de maneira específica. 
+Durante o evento, iremos utilizar conceitos e ferramentas de IA Generativa, consulta na Internet, LLM (Large Language Models), Python e suas Libs (OpenAI e LangChain). 
+
+O **objetivo** é formar um agente planejador de viagens, o qual há um usuário realizando um request (query + instructions), que passam pela Lib do LangChain (utilizando DuckDuckGo e Wikipedia para consultas) e o GPT no modelo LLM responde ao solicitado pelo usuário. 
+
+Para aumentar o campo de pesquisa do Chat, iremos fazer o método **RAG**, consultando um site de dicas de viagem. Para isso, pegamos o conteúdo do site, transformarmos em embedding model e salvamos em um banco vetorial, conseguindo acessar o conteúdo do site via LangChain.
+
+Além disso, também utilizamos o **Agente Supervisor** do GPT. Com ele podemos aplicar diretrizes específicas, com regras e comandos que aquele Agente vai executar da melhor forma esperada. 
--- travelAgent.py
+++ travelAgent.py
@@ -1,28 +1,75 @@
 import os
-from langchain_openai import ChatOpenAI
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.agent_toolkits.load_tools import load_tools
-from langchain.agents import initialize_agent 
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain import hub
+
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+import bs4
+
+from langchain_text_splitters.character import RecursiveCharacterTextSplitter
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableSequence
 
 llm = ChatOpenAI(model="gpt-3.5-turbo")
-
-# Carregar as ferramentas DuckDuckGo Search e Wikipedia
-tools = load_tools(['ddg-search', 'wikipedia'], llm=llm)
-
-# print(tools[0].name, tools[0].description)
-# print(tools[1].name, tools[1].description)
-
-agent = initialize_agent ( 
-    tools,
-    llm,
-    agent = 'zero-shot-react-description',
-    verbose = True
-) # Inicializar o agente
-
-print("--------")
-print(agent.agent.llm_chain.prompt.template)
 
 query = """
 Vou viajar para Londres em agosto de 2024. Quero que faça um roteiro de viagem para mim com os eventos que irão ocorrer na data da viagem e com o preço da passagem de São Paulo para Londres.
 """ # Definir uma query de pesquisa
 
-agent.run(query)
+# Isolar os métodos em uma função para o Agente de Pesquisa
+def reasearchAgent(query, llm):
+    tools = load_tools(['ddg-search', 'wikipedia'], llm=llm)
+    prompt = hub.pull("hwchase17/react")
+    agent = create_react_agent (llm, tools, prompt) # Inicializar o agente
+    agent_executor = AgentExecutor(agent=agent, tools=tools, prompt=prompt, verbose=True)
+    webContext = agent_executor.invoke({"input": query})
+    return webContext['output']
+
+def loadData():
+    loader = WebBaseLoader(
+        web_paths= ("https://www.dicasdeviagem.com/inglaterra",),
+        bs_kwargs=dict(parse_only=bs4.SoupStrainer(class_=("pagetitleloading background-imaged loading-dark", "postcontentwrap"))),
+    ) # Carregar a página e seu conteúdo
+    docs = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200) # Recurso para separar em pedaços menores
+    splits = text_splitter.split_documents(docs)
+    vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+    retriever = vectorstore.as_retriever()
+    return retriever
+
+def getRelevantDocs(query):
+    retriever = loadData()
+    relevant_documents = retriever.invoke(query)
+    print(relevant_documents)
+    return relevant_documents
+
+# Montagem da resposta final do Prompt com os detalhes necessários 
+def supervisorAgent(query, llm, webContext, relevant_documents):
+    prompt_template = """"
+    Você é um gerente de uma agência de viagens. Sua resposta final deverá ser um roteiro de viagem completo e detalhado. 
+    Utilize o contexto de eventos e preços de passagens, o input do usuário e também os documentos relevantes para elaboração.
+    Contexto: {webContext}
+    Documentos relevantes: {relevant_documents}
+    Usuário: {query}
+    Assistente:
+    """
+
+    prompt = PromptTemplate(
+        input_variables=['webContext', 'relevant_documents', 'query'],
+        template= prompt_template
+    )
+
+    sequence = RunnableSequence(prompt | llm)
+    response = sequence.invoke({"webContext": webContext, "relevant_documents": relevant_documents, "query": query})
+    return response
+
+
+def getResponse(query, llm):
+    webContext = reasearchAgent(query, llm)
+    relevant_documents = getRelevantDocs(query)
+    response = supervisorAgent(query, llm, webContext, relevant_documents)
+    return(response)
+
+print(getResponse(query, llm).content)
